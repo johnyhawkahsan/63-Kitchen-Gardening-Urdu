8c89e9d479a4bd23bbef4f835fc027f531ff3bcf
Ahsan <johnyhawkahsan@gmail.com>
2021-06-21 16:45:33 +0500

    Initial Commit- بسم اللہ الرحمٰن الرحیم

---
+++ app/src/main/java/com/johnyhawkdesigns/a63_kitchen_gardening_urdu/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.johnyhawkdesigns.a63_kitchen_gardening_urdu.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
